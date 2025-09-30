0e2ae35f0bf52e9307b72200ce7d0f3fd3ead981
ASH-INA <a.wesebebe@alustudent.com>
2025-10-01 00:21:30 +0300

    auth

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -7,10 +7,10 @@ from typing import List, TypeVar
 
 class Auth:
     """Template for all authentication systems"""
-    
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Determine if authentication is required
-        
+
         Args:
             path: The path to check
             excluded_paths: List of paths that don't require authentication
@@ -19,24 +19,24 @@ class Auth:
             False for now (will be implemented later)
         """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """Get the authorization header from the request
-        
+
         Args:
             request: The Flask request object
-            
+
         Returns:
             None for now (will be implemented later)
         """
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """Get the current user from the request
-        
+
         Args:
             request: The Flask request object
-            
+
         Returns:
             None for now (will be implemented later)
         """
