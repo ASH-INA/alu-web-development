64cbd7a934b4dd5206596174d408d150df95fe9e
ASH-INA <a.wesebebe@alustudent.com>
2025-10-01 00:55:28 +0300

    basic_auth

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -52,7 +52,9 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
         self, user_email: str, user_pwd: str
     ) -> TypeVar('User'):
-        """Get User instance based on email and password"""
+        """
+        Get User instance based on email and password
+        """
         if user_email is None or not isinstance(user_email, str):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
